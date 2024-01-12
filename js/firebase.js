f199ae9fb1422f7d12f6a31b7b280e724137e52f
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2024-01-12 17:38:53 +0000

    form added

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyB1UtgYYwIoXu2LiBkhz8nkSMRgl_bC69k",
+    authDomain: "tytlmap.firebaseapp.com",
+    projectId: "tytlmap",
+    storageBucket: "tytlmap.appspot.com",
+    messagingSenderId: "62499245628",
+    appId: "1:62499245628:web:637ff57a8e3a64b05c3d39"
+});
+
+var db = firebase.firestore();
