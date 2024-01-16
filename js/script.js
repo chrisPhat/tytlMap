c6fd4925a8ce72e39e3280c3624ed2f2bef8387c
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2024-01-16 16:14:20 +0000

    split for map

--- js/script.js
+++ js/script.js
@@ -3,26 +3,27 @@ var map = L.map('map').setView([54.3781, -2.8], 6);
 var pastLocations = [];
 var liveLocations = [];
 
-db.collection('shows').get().then((querysnapshot) =>{
+db.collection('shows').orderBy('showDate').get().then((querysnapshot) => {
     querysnapshot.forEach((doc) => {
         const dtStr = doc.data().showDate;
         const [d, m, y] = dtStr.split(/-|\//); // splits "26-02-2012" or "26/02/2012"
         const date = new Date(d, m - 1, y);
         //console.log(date.getTime() + " " + doc.data().venueName);
-        if(date.getTime() > Date.now()){
+        if (date.getTime() > Date.now()) {
             //console.log(doc.data());
             liveLocations.push(doc.data())
         } else {
             pastLocations.push(doc.data());
         }
-    //console.log(doc.data().showDate);
+        //console.log(doc.data().showDate);
     })
-}).then(()=>{
+}).then(() => {
+    console.log(liveLocations);
     pastLocations.forEach((location) => {
         //console.log(location.cityName);
         var marker = L.marker([location.latitude, location.longitude], { icon: pastIcon }).addTo(map);
     })
-    
+
     liveLocations.forEach((location) => {
         //console.log(location.cityName);
         var marker = L.marker([location.latitude, location.longitude], { icon: liveIcon })
@@ -32,7 +33,7 @@ db.collection('shows').get().then((querysnapshot) =>{
         <a href='${location.ticketLink}'>Click for Tickets</a>
         `);
     })
-   
+
 });
 
 
@@ -41,7 +42,7 @@ db.collection('shows').get().then((querysnapshot) =>{
 var pastIcon = L.icon({
     iconUrl: '../assets/pastShow.png',
 
-    iconSize: [50, 50], // size of the icon
+    iconSize: [25, 25], // size of the icon
     iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
 
 });
@@ -49,7 +50,7 @@ var pastIcon = L.icon({
 var liveIcon = L.icon({
     iconUrl: '../assets/liveShow.png',
 
-    iconSize: [60, 60], // size of the icon
+    iconSize: [40, 40], // size of the icon
     iconAnchor: [30, 30], // point of the icon which will correspond to marker's location
 
 });
